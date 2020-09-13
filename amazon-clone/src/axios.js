50ce286730985a2568a16445c76b94f0d70375e3
Van Huy Pham <vanhuycx@gmail.com>
2020-09-13 17:11:21 -0400

    deploy change

--- amazon-clone/src/axios.js
+++ amazon-clone/src/axios.js
@@ -1,7 +1,9 @@
 import axios from "axios";
 
 const instance = axios.create({
-  baseURL: "http://localhost:5001/clone-efe6b/us-central1/api", // the API (cloud function) URL
+  baseURL: "https://us-central1-clone-efe6b.cloudfunctions.net/api", // the API (cloud function) URL
 });
 
 export default instance;
+
+//http://localhost:5001/clone-efe6b/us-central1/api
